11e62d180a07459e395cd66835b8433b3530b1a1
Nam Nguyen <namdoannguyen@mgial.com>
2020-08-14 14:41:53 -0700

    lazyloading text and infinite scroll

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,102 @@
-import React, { useEffect, useState } from 'react';
-import TextItem from './TextItem'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import BottomScrollListener from "react-bottom-scroll-listener";
+
+import Row from "./Row/Row";
+import "./App.css";
 
 // const DATA_SIZE_HALF = "half"
-const DATA_SIZE_FULL = "full"
-const INTERVAL_TIME = 2000
+const DATA_SIZE_FULL = "full";
+const INTERVAL_TIME = 2000;
+
+// determine how many more paragraphs we want to fetch on scroll to botton
+const INFINITE_SCROLL_INCREMENTER = 5;
 
 /** Application entry point */
 function App() {
+  const [data, setData] = useState([]);
+  const [ids, setIds] = useState([]);
 
-  const [data, setData] = useState([])
-  const [value, setValue] = useState(0)
-  const [searchInput, setSearchInput] = useState("")
+  const [value, setValue] = useState(0);
+  const [searchInput, setSearchInput] = useState("");
+
+  // we can use lower and upper limits to implement infinite scrolling - 2 items at a time
+  const [lower, setLower] = useState(0);
+  const [upper, setUpper] = useState(5);
 
   /** DO NOT CHANGE THE FUNCTION BELOW */
   useEffect(() => {
     setInterval(() => {
       // Find random bucket of words to highlight
-      setValue(Math.floor(Math.random() * 10))
-    }, INTERVAL_TIME)
-  }, [])
+      setValue(Math.floor(Math.random() * 10));
+    }, INTERVAL_TIME);
+  }, []);
   /** DO NOT CHANGE THE FUNCTION ABOVE */
 
   useEffect(() => {
+    const fetchIds = async () => {
+      let response = await fetch("/api/dataIdList?datasize=" + DATA_SIZE_FULL);
+      // this will return a list of ids
+      let list = await response.json();
+      setIds(list);
+    };
+
+    fetchIds();
+  }, []);
+
+  useEffect(() => {
     const fetchData = async () => {
-      let response = await fetch("/api/dataIdList?datasize=" + DATA_SIZE_FULL)
-      let list = await response.json()
+      if (ids.length) {
+        // get range of id's from lower to upper limit
+        let currentItems = [...ids].slice(lower, upper);
 
-      let dataItems = await Promise.all (list.map(async id => {
-        return (await fetch("/api/dataItem/" + id)).json()
-      }))
-      setData(dataItems)
-    }
-    
-    fetchData()
-  }, [])
+        // fetch each line based on id
+        let dataItems = await Promise.all(
+          currentItems.map(async (id) => {
+            return (await fetch("/api/dataItem/" + id)).json();
+          })
+        );
 
-  const handleChange = e => {
-    setSearchInput(e.target.value)
-  }
+        // need to use concat or else screen will be stuck at scroll bottom and endlessly render
+        setData((data) => data.concat(dataItems));
+      }
+    };
+
+    fetchData();
+  }, [ids, lower]);
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+  // callback to be used when scroll bottom is hit
+  const onBottom = () => {
+    let temp = upper;
+    setUpper((upper) => (upper += INFINITE_SCROLL_INCREMENTER));
+    setLower(temp);
+  };
 
   return (
     <div className="App">
-      <h2>JT Online Book</h2>
-      <div>
-        <input type="text" placeholder="Search text" value={searchInput} onChange={handleChange}/>
+      <h2 className="heading">JusticeText Online Book</h2>
+      <div className="searchbar">
+        <input
+          className="input"
+          type="text"
+          placeholder="Search text"
+          value={searchInput}
+          onChange={handleChange}
+        />
       </div>
-     {
-       data.map((row, i) => {
-        return (<p key={`p${i}`}>
-          {row.map((textitem, j) => {
-            if (searchInput.length > 0 && textitem.text.search(searchInput) === -1) {
-              return null;
-            }
-
-            return (
-            <>
-              <TextItem key={`${i}${j}`} value={value} data={textitem}/>
-            </>)
-          })}
-        </p>)
-       })
-     }
+      {data.map((row, i) => {
+        return (
+          <Row
+            key={`p${i}`}
+            row={row}
+            value={value}
+            searchInput={searchInput}
+          />
+        );
+      })}
+      <BottomScrollListener onBottom={onBottom} />
     </div>
   );
 }
